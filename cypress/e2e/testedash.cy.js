1cc408f9d20d98aec4728b39101fcd002fe10b78
Lucas de Souza Resende <99922083+LucasSResende@users.noreply.github.com>
2023-05-11 20:53:37 -0300

    Criação de testes iniciais

--- cypress/e2e/testedash.cy.js
+++ cypress/e2e/testedash.cy.js
@@ -1,5 +1,24 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("Acesso ao site e teste de entradas em 'Entries'", () => {
+  it("Testes iniciais", () => {
+    cy.viewport(1920, 1080);
     cy.visit("http://localhost:4200/");
+    cy.get('[routerlink="/entry"]').click();
+    cy.get(".newEntryButton > .mdc-button__label > span").click();
+    cy.get("#mat-input-0").type("Lucas de Souza Resende");
+    cy.get("#mat-input-1").type("05/05/2023");
+    cy.get("#mat-input-2").type("1115,33");
+    cy.get(".mat-mdc-select-placeholder").click();
+    cy.get("#mat-option-0").click();
+    cy.get("#mat-input-3").click();
+    cy.get("#mat-input-3").type("Despesas diversas");
+    cy.get(".confirmButton > .mdc-button__label > span").click();
+  });
+});
+
+describe.skip("Mudança de tela", () => {
+  it("Tela type e inserção de valores", () => {
+    cy.viewport(1920, 1080);
+    cy.visit("http://localhost:4200/");
+    cy.get('[routerlink="/type"]').click();
   });
 });
