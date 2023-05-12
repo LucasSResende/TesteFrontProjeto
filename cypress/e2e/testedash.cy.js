f6e01081074bc0d61e40aad7f129f91344930deb
Lucas de Souza Resende <99922083+LucasSResende@users.noreply.github.com>
2023-05-12 12:12:17 -0300

    Testes de cada tela

--- cypress/e2e/testedash.cy.js
+++ cypress/e2e/testedash.cy.js
@@ -1,5 +1,5 @@
 describe("Acesso ao site e teste de entradas em 'Entries'", () => {
-  it("Testes iniciais", () => {
+  it.skip("Testes iniciais", () => {
     cy.viewport(1920, 1080);
     cy.visit("http://localhost:4200/");
     cy.get('[routerlink="/entry"]').click();
@@ -15,10 +15,24 @@ describe("Acesso ao site e teste de entradas em 'Entries'", () => {
   });
 });
 
-describe.skip("Mudança de tela", () => {
-  it("Tela type e inserção de valores", () => {
+describe("Mudança de tela", () => {
+  it.skip("Tela type e inserção de valores", () => {
     cy.viewport(1920, 1080);
     cy.visit("http://localhost:4200/");
     cy.get('[routerlink="/type"]').click();
+    cy.get(".mdc-button__label > span").click();
+    cy.get("#mat-input-0").type("Food");
+    cy.get("#mat-input-1").type("28");
+    cy.get(".confirm-button > .mdc-button__label > span").click();
+  });
+});
+
+describe("Voltando para o dashboard", () => {
+  it("Tela type e inserção de valores", () => {
+    cy.viewport(1920, 1080);
+    cy.visit("http://localhost:4200/");
+    cy.get(
+      '[routerlink="/"] > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .center'
+    ).click();
   });
 });
