a404ee418a0aeae1ec2136364681e30108bb9918
Lucas de Souza Resende <99922083+LucasSResende@users.noreply.github.com>
2023-05-24 21:18:41 -0300

    Atualizações dos testes cypress

--- cypress/e2e/testedash.cy.js
+++ cypress/e2e/testedash.cy.js
@@ -1,7 +1,6 @@
 describe("Acesso ao site e teste de entradas em 'Entries'", () => {
-  it.skip("Testes iniciais", () => {
-    cy.viewport(1920, 1080);
-    cy.visit("http://localhost:4200/");
+  it("Testes iniciais de uma entrada em 'Entries'", () => {
+    cy.wrap(login());
     cy.get('[routerlink="/entry"]').click();
     cy.get(".newEntryButton > .mdc-button__label > span").click();
     cy.get("#mat-input-0").type("Lucas de Souza Resende");
@@ -15,10 +14,37 @@ describe("Acesso ao site e teste de entradas em 'Entries'", () => {
   });
 });
 
-describe("Mudança de tela", () => {
-  it.skip("Tela type e inserção de valores", () => {
-    cy.viewport(1920, 1080);
-    cy.visit("http://localhost:4200/");
+describe("Delete 'Entries'", () => {
+  it("Testes para apagar um 'Entries'", () => {
+    cy.wrap(login());
+    cy.get('[routerlink="/entry"]').click();
+    cy.get(
+      ":nth-child(11) > .cdk-column-action > .delete > .material-icons"
+    ).click();
+    cy.get(".confirmButton > .mdc-button__label > span").click();
+  });
+});
+
+describe("Edit 'Entries'", () => {
+  it("Teste de edição em uma entrada existente em 'Entries'", () => {
+    cy.wrap(login());
+    cy.get('[routerlink="/entry"]').click();
+    cy.get(
+      '[title="Edit"] > .b > .back > .entry-list > .table > .mat-mdc-table > .mdc-data-table__content > :nth-child(1) > .cdk-column-action > .edit > .material-icons'
+    ).click();
+    cy.get("#mat-input-0").clear();
+    cy.get("#mat-input-0").type("Eletric bills");
+    cy.get("#mat-input-2").clear();
+    cy.get("#mat-input-2").type("298.2");
+    cy.get("#mat-input-3").clear();
+    cy.get("#mat-input-3").type("Readjustment of values");
+    cy.get(".confirmButton > .mdc-button__label > span").click();
+  });
+});
+
+describe("Teste na tela de tipos de entradas", () => {
+  it("Inserção de um novo 'Type'", () => {
+    cy.wrap(login());
     cy.get('[routerlink="/type"]').click();
     cy.get(".mdc-button__label > span").click();
     cy.get("#mat-input-0").type("Food");
@@ -27,12 +53,45 @@ describe("Mudança de tela", () => {
   });
 });
 
-describe("Voltando para o dashboard", () => {
-  it("Tela type e inserção de valores", () => {
-    cy.viewport(1920, 1080);
-    cy.visit("http://localhost:4200/");
+describe("Verificando valores de entrada no budget", () => {
+  it("Tela budget e inserção de valores", () => {
+    cy.wrap(login());
     cy.get(
-      '[routerlink="/"] > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .center'
+      '[routerlink="/budget"] > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .center > span.grey'
     ).click();
+    cy.get("#mat-input-0").type("350.0");
+    cy.get("#mat-input-1").type("62.5");
+    cy.get("#mat-input-2").should("have.value", "287.5");
   });
 });
+
+describe("Verificando botão de entrada rápida", () => {
+  it("Pode ser de qualquer tela e deve inserir nova entrada de valores", () => {
+    cy.wrap(login());
+    cy.get(".container > :nth-child(1) > .mat-mdc-button-touch-target").click();
+    cy.get("#mat-input-0").type("New expensive bill");
+    cy.get("#mat-input-1").type("25/05/2023");
+    cy.get("#mat-input-2").type("12825.3");
+    cy.get(".mat-mdc-select-placeholder").click();
+    cy.get("#mat-option-1").click();
+    cy.get("#mat-input-3").type("New notebook aquisition");
+    cy.get(".confirmButton > .mdc-button__label > span").click();
+  });
+});
+
+describe("Verificando entrada de valores e valor final cálculo de 'Real Incomming", () => {
+  it("Comparando cálculo final se está correto", () => {
+    cy.wrap(login());
+    cy.get(
+      '[routerlink="/budget"] > .mdc-list-item__content > .mat-mdc-list-item-unscoped-content > .center > span.grey'
+    ).click();
+    cy.get("#mat-input-0").type("350.0");
+    cy.get("#mat-input-1").type("62.5");
+    cy.get("#mat-input-2").should("have.value", "287.5");
+  });
+});
+
+function login() {
+  cy.viewport(1920, 1080);
+  cy.visit("http://localhost:4200/");
+}
