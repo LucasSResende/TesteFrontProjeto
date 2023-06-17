7214d8ef8bafa7f17bc2e6e819093e8917d7c608
LucasSResende <lucasresendesrs@gmail.com>
2023-06-17 16:41:59 -0300

    Atualização de testes

--- cypress/e2e/testedash.cy.js
+++ cypress/e2e/testedash.cy.js
@@ -10,19 +10,19 @@ describe("Acesso ao site e teste de criação de 'New type'", () => {
     cy.get('[routerlink="/type"]').click();
     cy.get(".mdc-button__label > span").click();
     cy.get("#mat-input-0").type("teste");
-    cy.get("#mat-input-1").type("21");
+    cy.get("#mat-input-1").type("15");
     cy.get(".confirm-button > .mdc-button__label > span").click();
-
+    cy.wait(1000);
     cy.get('[routerlink="/type"]').click();
     cy.get(".mdc-button__label > span").click();
     cy.get("#mat-input-0").type("basic");
-    cy.get("#mat-input-1").type("35");
+    cy.get("#mat-input-1").type("20");
     cy.get(".confirm-button > .mdc-button__label > span").click();
-
+    cy.wait(1000);
     cy.get('[routerlink="/type"]').click();
     cy.get(".mdc-button__label > span").click();
-    cy.get("#mat-input-0").type("Food");
-    cy.get("#mat-input-1").type("28");
+    cy.get("#mat-input-0").type("Laisure");
+    cy.get("#mat-input-1").type("39");
     cy.get(".confirm-button > .mdc-button__label > span").click();
   });
 });
@@ -60,6 +60,16 @@ describe("Criar um 'New Entry'", () => {
     cy.get("#mat-option-1").click();
     cy.get("#mat-input-3").type("monthly supermarket");
     cy.get(".confirmButton > .mdc-button__label > span").click();
+    cy.wait(1000);
+    cy.get('[routerlink="/entry"]').click();
+    cy.get(".newEntryButton > .mdc-button__label > span").click();
+    cy.get("#mat-input-0").type("Viagem litoral");
+    cy.get("#mat-input-1").type("07/10/2023");
+    cy.get("#mat-input-2").type("3280,00");
+    cy.get("svg.ng-tns-c55-6 > .ng-tns-c55-6").click();
+    cy.get("#mat-option-1").click();
+    cy.get("#mat-input-3").type("Vacation's Travel");
+    cy.get(".confirmButton > .mdc-button__label > span").click();
   });
 });
 
@@ -75,7 +85,7 @@ describe("Teste na tela de tipos de entradas", () => {
     cy.wrap(login());
     cy.get('[routerlink="/type"]').click();
     cy.get(
-      ":nth-child(3) > .cdk-column-action > .delete > .material-icons"
+      ":nth-child(2) > .cdk-column-action > .delete > .material-icons"
     ).click();
     cy.get(".confirm-button > .mdc-button__label > span").click();
   });
@@ -114,10 +124,10 @@ describe("Edit 'Entries'", () => {
     cy.wrap(login());
     cy.get('[routerlink="/entry"]').click();
     cy.get(
-      ":nth-child(1) > .cdk-column-action > .edit > .material-icons"
+      ":nth-child(2) > .cdk-column-action > .edit > .material-icons"
     ).click();
     cy.get("#mat-input-0").clear();
-    cy.get("#mat-input-0").type("Tool kit 1");
+    cy.get("#mat-input-0").type("Tool kit");
     cy.get("#mat-input-2").clear();
     cy.get("#mat-input-2").type("135.25");
     cy.get("#mat-input-3").clear();
@@ -134,16 +144,14 @@ describe("Edição de 'Types'", () => {
     cy.get(
       ":nth-child(2) > .cdk-column-action > .edit > .material-icons"
     ).click();
-    cy.get("#mat-input-0").clear();
-    cy.get("#mat-input-0").type("Basic");
     cy.get("#mat-input-1").clear();
-    cy.get("#mat-input-1").type("40");
+    cy.get("#mat-input-1").type("22");
     cy.get(".confirm-button > .mdc-button__label > span").click();
     cy.wrap(login());
   });
 });
 
-describe.skip("Error 'Entries'", () => {
+describe("Edição de 'Entries'", () => {
   it("Teste de edição em uma entrada existente em 'Entries'", () => {
     cy.wrap(login());
     cy.get('[routerlink="/entry"]').click();
@@ -155,13 +163,56 @@ describe.skip("Error 'Entries'", () => {
     cy.get("#mat-input-2").clear();
     cy.get("#mat-input-2").type("135.25");
     cy.get("#mat-input-3").clear();
-    cy.get("#mat-input-3").type("Real values");
+    cy.get("#mat-input-3").type("Price increase");
     cy.get(".confirmButton > .mdc-button__label > span").click();
-    cy.wrap(login());
   });
 });
 
-//cy.get('.mat-mdc-simple-snack-bar > .mat-mdc-snack-bar-label').should('have.text', 'Ocorreu um erro!');
+describe("Teste tela 'Entries'", () => {
+  it("Testes de asserts quanto aos campos 'Name', 'Date', 'Value', 'Classification' e 'Description'", () => {
+    cy.wrap(login());
+    cy.get('[routerlink="/entry"]').click();
+    cy.get(
+      ".mdc-data-table__content > :nth-child(1) > .cdk-column-name"
+    ).should("have.text", "New expensive bill");
+    cy.wait(1000);
+    cy.get('[routerlink="/entry"]').click();
+    cy.get(":nth-child(2) > .cdk-column-date").should(
+      "have.text",
+      "15/06/2023"
+    );
+    cy.wait(1000);
+    cy.get('[routerlink="/entry"]').click();
+    cy.get(":nth-child(3) > .cdk-column-value").should(
+      "contain.text",
+      "135,25"
+    );
+
+    cy.get('[routerlink="/entry"]').click();
+    cy.get(":nth-child(2) > .cdk-column-classification").should(
+      "have.text",
+      "basic"
+    );
+    cy.wait(1000);
+    cy.get('[routerlink="/entry"]').click();
+    cy.get(
+      ".mdc-data-table__content > :nth-child(1) > .cdk-column-description"
+    ).should("have.text", "New notebook aquisition");
+  });
+});
+
+describe("Teste tela 'Types'", () => {
+  it("Testes de asserts quanto aos campos 'Name' e 'Portion'", () => {
+    cy.wrap(login());
+    cy.get('[routerlink="/type"]').click();
+    cy.get(
+      ".mdc-data-table__content > :nth-child(1) > .cdk-column-name"
+    ).should("have.text", "teste");
+    cy.wait(1000);
+    cy.get('[routerlink="/type"]').click();
+    cy.get(":nth-child(2) > .cdk-column-portion").should("have.text", "39%");
+  });
+});
 
 function login() {
   cy.viewport(1920, 1080);
